0b46206a80e554e687032121051fabd01ec0dcd9
Arcade-helper <arcadehelper1418@gmail.com>
2025-04-12 05:43:47 +0530

    Update GSP602.sh

--- GSP602.sh
+++ GSP602.sh
@@ -12,7 +12,9 @@ gcloud services enable cloudfunctions.googleapis.com
 gcloud services enable run.googleapis.com
 
 curl -LO https://github.com/GoogleCloudPlatform/golang-samples/archive/main.zip
+
 unzip main.zip
+
 cd golang-samples-main/functions/codelabs/gopher
 
 gcloud functions deploy HelloWorld --gen2 --runtime go121 --trigger-http --region $REGION --allow-unauthenticated
