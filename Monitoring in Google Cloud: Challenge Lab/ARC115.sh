b90262064c1322989b006b041d9b1e78d5dcbb86
Arcade-helper <arcadehelper1418@gmail.com>
2025-04-29 21:56:41 +0530

    Create ARC115.sh

---
+++ Monitoring in Google Cloud: Challenge Lab/ARC115.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Define color variables
+
+YELLOW_TEXT=$'\033[0;93m'
+RESET_FORMAT=$'\033[0m'
+clear
+
+
+export ZONE=$(gcloud compute instances list --project=$DEVSHELL_PROJECT_ID --format='value(ZONE)' | head -n 1)
+INSTANCE_ID=$(gcloud compute instances describe apache-vm --zone=$ZONE --format='value(id)')
+
+cat > cp_disk.sh <<'EOF_CP'
+curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
+sudo bash add-logging-agent-repo.sh --also-install
+
+curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh
+sudo bash add-monitoring-agent-repo.sh --also-install
+
+(cd /etc/stackdriver/collectd.d/ && sudo curl -O https://raw.githubusercontent.com/Stackdriver/stackdriver-agent-service-configs/master/etc/collectd.d/apache.conf)
+
+sudo service stackdriver-agent restart
+EOF_CP
+
+gcloud compute scp cp_disk.sh apache-vm:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
+
+gcloud compute ssh apache-vm --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/cp_disk.sh"
+
+gcloud monitoring uptime create arcadecrew \
+  --resource-type="gce-instance" \
+  --resource-labels=project_id=$DEVSHELL_PROJECT_ID,instance_id=$INSTANCE_ID,zone=$ZONE
+
+cat > email-channel.json <<EOF_CP
+{
+  "type": "email",
+  "displayName": "arcadecrew",
+  "description": "Arcade Crew",
+  "labels": {
+    "email_address": "$USER_EMAIL"
+  }
+}
+EOF_CP
+
+gcloud beta monitoring channels create --channel-content-from-file="email-channel.json"
+
+channel_info=$(gcloud beta monitoring channels list)
+channel_id=$(echo "$channel_info" | grep -oP 'name: \K[^ ]+' | head -n 1)
+
+cat > app-engine-error-percent-policy.json <<EOF_CP
+{
+  "displayName": "alert",
+  "userLabels": {},
+  "conditions": [
+    {
+      "displayName": "VM Instance - Traffic",
+      "conditionThreshold": {
+        "filter": "resource.type = \"gce_instance\" AND metric.type = \"agent.googleapis.com/apache/traffic\"",
+        "aggregations": [
+          {
+            "alignmentPeriod": "60s",
+            "crossSeriesReducer": "REDUCE_NONE",
+            "perSeriesAligner": "ALIGN_RATE"
+          }
+        ],
+        "comparison": "COMPARISON_GT",
+        "duration": "300s",
+        "trigger": {
+          "count": 1
+        },
+        "thresholdValue": 3072
+      }
+    }
+  ],
+  "alertStrategy": {
+    "autoClose": "1800s"
+  },
+  "combiner": "OR",
+  "enabled": true,
+  "notificationChannels": [
+    "$channel_id"
+  ],
+  "severity": "SEVERITY_UNSPECIFIED"
+}
+EOF_CP
+
+gcloud alpha monitoring policies create --policy-from-file="app-engine-error-percent-policy.json"
