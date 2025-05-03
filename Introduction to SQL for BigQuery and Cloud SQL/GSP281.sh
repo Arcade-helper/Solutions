35a4c6f723e9aee0125678a25b168e69377711e6
Arcade-helper <arcadehelper1418@gmail.com>
2025-05-04 03:25:52 +0530

    Update GSP281.sh

--- Introduction to SQL for BigQuery and Cloud SQL/GSP281.sh
+++ Introduction to SQL for BigQuery and Cloud SQL/GSP281.sh
@@ -1,7 +1,7 @@
 gsutil mb gs://$DEVSHELL_PROJECT_ID
 
-curl -O 
-curl -O 
+curl -O https://github.com/Arcade-helper/Solutions/blob/main/Introduction%20to%20SQL%20for%20BigQuery%20and%20Cloud%20SQL/start_station_name.csv
+curl -O https://github.com/Arcade-helper/Solutions/blob/main/Introduction%20to%20SQL%20for%20BigQuery%20and%20Cloud%20SQL/end_station_name.csv
 
 gsutil cp start_station_name.csv gs://$DEVSHELL_PROJECT_ID/
 gsutil cp end_station_name.csv gs://$DEVSHELL_PROJECT_ID/
