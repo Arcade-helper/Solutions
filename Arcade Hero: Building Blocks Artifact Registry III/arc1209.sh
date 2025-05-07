36f3fe31529f7936735f2ce3eb957be1c9af8700
Arcade-helper <arcadehelper1418@gmail.com>
2025-05-08 00:10:43 +0530

    Create arc1209.sh

---
+++ Arcade Hero: Building Blocks Artifact Registry III/arc1209.sh
@@ -0,0 +1,14 @@
+ export REGION=$(gcloud compute project-info describe \
+ --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+gcloud services enable artifactregistry.googleapis.com
+
+gcloud artifacts repositories create container-registry \
+  --repository-format=docker \
+  --location=$REGION \
+  --description="Docker repository in $REGION"
+
+ gcloud artifacts repositories create go-registry \
+  --repository-format=go \
+  --location=$REGION \
+  --description="Go module repository"
